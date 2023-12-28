5984a755d0e0a1ef4c02680df5eda1c5d15667a8
tam.nguyen <tam.nguyen@bnksolution.com>
2023-12-28 13:29:05 +0700

    modify something

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -21,7 +21,7 @@ export class Counter extends Component {
 					<span
 						class="outline-none focus:outline-none justify-center text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black md:text-basecursor-default flex items-center text-gray-700 outline-none"
 						x-text="count"
-					></span>
+					/>
 					<button
 						x-on:click="count++"
 						class="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer"
